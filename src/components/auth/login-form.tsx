f3f0bf5654a9231f35b6b01b7b596a85eae4bf69
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-14 18:17:33 +0530

    fixed login form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -3,9 +3,9 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from '../../store/authSlice'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 
 export default function LoginForm() {
   const dispatch = useDispatch()
