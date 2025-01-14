f05964944749a38078ac6de6aaa85b423d2d7adc
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-14 18:24:42 +0530

    added theme provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,20 @@
 
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
+import { ThemeProvider } from "next-themes"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
